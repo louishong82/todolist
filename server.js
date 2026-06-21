59e5c1bef06d4a88083c048f12264a3e0268e8e4
louis <louis.082h@gmail.com>
2026-06-21 20:21:39 +0800

    headers,successHandle 模組化,排除空字串,判斷嚴格相等

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const http = require('http');
 const {v4: uuidv4 } = require('uuid')
 const errorHandle = require('./errorHandle');
+const headers = require('./headers');
+const successHandle = require('./successHandle');
 const todos = []
 
 const requestListener = (req , res) => {
-    const headers = {
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-    'Content-Type': 'application/json'
-    }
     let body = "";
     req.on('data', chunk =>{
         body+=chunk;
@@ -17,29 +13,19 @@ const requestListener = (req , res) => {
     
     console.log(req.url);
     console.log(req.method);
-    if(req.url == "/todos" && req.method == "GET"){
-        res.writeHead(200,headers);
-        res.write(JSON.stringify({
-            "status":"success",
-            "data": todos,
-        }));
-        res.end();
-    }else if(req.url == "/todos" && req.method == "POST"){
+    if(req.url === "/todos" && req.method === "GET"){
+        successHandle(res,todos)
+    }else if(req.url === "/todos" && req.method === "POST"){
         req.on('end',()=>{
             try{
-                const title = JSON.parse(body).title
+                const title = JSON.parse(body).title.trim()
                 if(title){
                     const todo = {
                         "title": title,
                         "id": uuidv4(),
                     }
                     todos.push(todo);
-                    res.writeHead(200,headers);
-                    res.write(JSON.stringify({
-                        "status":"success",
-                        "data": todos,
-                    }));
-                    res.end();
+                    successHandle(res,todos);
                 }else{
                     errorHandle(res)
                 }
@@ -48,42 +34,27 @@ const requestListener = (req , res) => {
             }
             
         })
-    }else if(req.url == "/todos" && req.method == "DELETE"){
+    }else if(req.url === "/todos" && req.method === "DELETE"){
         todos.length = 0
-        res.writeHead(200,headers);
-        res.write(JSON.stringify({
-            "status":"success",
-            "data": todos,
-        }));
-        res.end();
-    }else if(req.url.startsWith("/todos/") && req.method == "DELETE"){
+        successHandle(res,todos);
+    }else if(req.url.startsWith("/todos/") && req.method === "DELETE"){
         const id = req.url.split("/").pop();
-        const index = todos.findIndex(element => element.id == id) ;
+        const index = todos.findIndex(element => element.id === id) ;
         if(index !== -1){
             todos.splice(index,1);
-            res.writeHead(200,headers);
-            res.write(JSON.stringify({
-                "status":"success",
-                "data": todos,
-            }));
-            res.end();
+            successHandle(res,todos);
         }else{
             errorHandle(res);
         }
-    }else if(req.url.startsWith("/todos/") && req.method == "PATCH"){
+    }else if(req.url.startsWith("/todos/") && req.method === "PATCH"){
         req.on('end',()=>{
             try{
-                const todo = JSON.parse(body).title;
+                const title = JSON.parse(body).title.trim();
                 const id = req.url.split('/').pop();
-                const index = todos.findIndex(element => element.id == id)
-                if(todo && index !== -1){
-                    todos[index].title = todo;
-                    res.writeHead(200,headers);
-                    res.write(JSON.stringify({
-                        "status":"success",
-                        "data": todos,
-                    }));
-                    res.end();
+                const index = todos.findIndex(element => element.id === id)
+                if(title && index !== -1){
+                    todos[index].title = title;
+                    successHandle(res,todos);
                 }else{
                     errorHandle(res);
                 }
@@ -91,7 +62,7 @@ const requestListener = (req , res) => {
                 errorHandle(res);
             }
         })
-    }else if(req.method == "OPTIONS"){
+    }else if(req.method === "OPTIONS"){
         res.writeHead(200,headers);
         res.end();
     }else{
---
+++ successHandle.js
@@ -0,0 +1,12 @@
+const headers = require('./headers')
+
+function successHandle(res,todos){
+    res.writeHead(200,headers);
+    res.write(JSON.stringify({
+        "status":"success",
+        "data": todos,
+    }));
+    res.end();
+}        
+
+module.exports = successHandle ;
